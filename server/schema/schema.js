fded1ebe8f2fa290de521fa9c4084317a3f13d7d
Fiora De Marco <fiorademarco7@gmail.com>
2023-03-11 19:11:31 -0500

    added more queries and associations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,20 +1,42 @@
 const graphql = require('graphql')
 const _ = require('lodash')
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
+const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt} = graphql
 
 // dummy data
 var books = [
-    {name: 'Gone with the Wind', genre: 'Fantasy', id: '1'},
-    {name: 'The Final Empire', genre: 'Fantasy', id: '2'},
-    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3'},
+    {name: 'Gone with the Wind', genre: 'Historical Fiction', id: '1', authorId: '1'},
+    {name: 'The Final Empire', genre: 'Fantasy', id: '2', authorId: '2'},
+    {name: 'The Long Earth', genre: 'Sci-Fi', id: '3', authorId: '3'},
+]
+
+var authors = [
+    { name: 'Margaret Mitchell', age: '48', id: '1'},
+    { name: 'Brandon Sanderson', age: '42', id: '2'},
+    { name: 'Terry Pratchett', age: '66', id: '3'},
 ]
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID},
         name: {type: GraphQLString},
-        genre: {type: GraphQLString}
+        genre: {type: GraphQLString},
+        author: {
+            type: AuthorType,
+            resolve(parent, args) {
+                console.log(parent)
+                return _.find(authors, { id: parent.authorId})
+            }
+        }
+    })
+})
+
+const AuthorType = new GraphQLObjectType({
+    name: 'Author',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt},
     })
 })
 
@@ -23,14 +45,24 @@ const RootQuery = new graphql.GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: { id: {type:GraphQLString}},
+            args: { id: {type:GraphQLID}},
             resolve(parent, args) {
                 // code to get data from db / other source - Parent will come into play when we start to look at relationships between data
+                console.log(typeof(args.id))
                 return _.find(books, { id: args.id });
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {id: {type: GraphQLID}},
+            resolve(parent, args) {
+                return _.find(authors, { id: args.id})
             }
         }
     }
 });
+
+// run <npx nodemon app>
 
 module.exports = new graphql.GraphQLSchema({
     query: RootQuery
